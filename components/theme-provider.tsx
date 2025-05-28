2fc604aa7bbeb449ec78bea0b8a93c31ac750cbf
CaringClarity <mrsjessrenee@gmail.com>
2025-05-28 17:52:36 -0400

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -4,7 +4,11 @@ import * as React from 'react'
 import {
   ThemeProvider as NextThemesProvider,
   type ThemeProviderProps,
+  useTheme as useNextTheme,
 } from 'next-themes'
+
+// Re-export useTheme from next-themes
+export const useTheme = useNextTheme
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
